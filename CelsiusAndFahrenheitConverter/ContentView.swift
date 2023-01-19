8a7730badc66c3382ba919c0aa7af0ce64e8f4a2
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-19 16:21:09 -0500

    写了

--- CelsiusAndFahrenheitConverter/ContentView.swift
+++ CelsiusAndFahrenheitConverter/ContentView.swift
@@ -9,13 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+            // MARK: Stored properties
+            @State var degreeInCelsius: Double = 17
+        
+            @State var degreeInFahrenheit: Double = 70
+           
+           // MARK: Computed Properties
+           var fahrenheit: Double {
+               return (9 / 5) * degreeInCelsius + 32
+           }
+           
+           var calcius: Double {
+               return (degreeInFahrenheit - 32) / (9 / 5)
+               
+               
+               var fahrenheit: Double {
+                   return (9 / 5) * degreeInCelsius + 32
+               }
+               
+               var calcius: Double {
+                   return (degreeInFahrenheit - 32) / (9 / 5)
+               }
+               //User interfaces
+               var body: some View {
+                   VStack {
+                       
+                       VStack {
+                           Text("Celsius to Fahrenheit")
+                               .font(.title2)
+                               .bold()
+                           
+                           Slider(value: $degreeInCelsius,
+                                  in: 0...100,
+                                  label: { Text("degreeInCelsius") },
+                                  minimumValueLabel: { Text("0") },
+                                  maximumValueLabel: { Text("100") })
+                           .padding(.horizontal, 30)
+                           
+                           Text("Celsius: " + "\(degreeInCelsius)")
+                           
+                           Text("Fahrenheit: " + "\(fahrenheit)")
+                       }
+                       .padding(.bottom, 50.0)
+                       
+                       
+                       
+                       
+                       VStack {
+                           Text("Fahrenheit to Celsius")
+                               .font(.title2)
+                               .bold()
+                           
+                           Slider(value: $degreeInFahrenheit,
+                                  in: 0...100,
+                                  label: { Text("degreeInFahrenheit") },
+                                  minimumValueLabel: { Text("0") },
+                                  maximumValueLabel: { Text("100") })
+                           .padding(.horizontal, 30.0)
+                           
+                           Text("Celsius: " + "\(degreeInCelsius)")
+                           
+                           Text("Fahrenheit: " + "\(fahrenheit)")
+                       }
+                       .padding(.bottom, 50.0)
+                       
+                   }
+               }
+               
+               struct ContentView_Previews: PreviewProvider {
+                   static var previews: some View {
+                       ContentView()
+                   }
+               }
+           }
     }
 }
 
@@ -24,3 +91,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
