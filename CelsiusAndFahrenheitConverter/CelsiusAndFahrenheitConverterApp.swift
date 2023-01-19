1a27a1fd4af870df760325b3067a8cc6683bfcea
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-19 16:08:59 -0500

    Initial Commit

---
+++ CelsiusAndFahrenheitConverter/CelsiusAndFahrenheitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelsiusAndFahrenheitConverterApp.swift
+//  CelsiusAndFahrenheitConverter
+//
+//  Created by dyhidrogen monoxide on 2023/1/19.
+//
+
+import SwiftUI
+
+@main
+struct CelsiusAndFahrenheitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
